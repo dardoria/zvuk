bc46c6b7032d617f9207e9cbc21a186a398c32e3
Boian Tzonev <boiantz@gmail.com>
2012-09-18 07:15:36 +0300

    Rename the constructors in the form make-*

--- mus.lisp
+++ mus.lisp
@@ -9,11 +9,11 @@
   (let ((len (or size (length fn))))
     (if (< x 0.0) (incf x len))
     (multiple-value-bind
-	(int-part frac-part) 
+	(int-part frac-part)
 	(truncate x)
       (if (>= int-part len)
 	  (setf int-part (mod int-part len)))
-      (if (zerop frac-part) 
+      (if (zerop frac-part)
 	  (aref fn int-part)
 	(+ (aref fn int-part)
 	   (* frac-part (- (aref fn (if (< (1+ int-part) len) (1+ int-part) 0))
@@ -28,7 +28,7 @@
     rdat))
 
 ;;;;delay
-(defstruct delay
+(defstruct (delay (:constructor %make-delay (size line loc zloc zsize zdly type)))
   size
   line
   (loc 0)
@@ -40,33 +40,33 @@
   (yscl 0.0)
   (type :mus-interp-none))
 
-(defun new-delay (size initial-contents initial-element max-size type)
+(defun make-delay (size initial-contents initial-element max-size type)
   (let ((lsize (round (or max-size size))))
-    (make-instance 'delay
-     :loc 0
-     :size (floor size)
-     :zsize lsize
-     :zdly max-size
-     :zloc (and max-size (- max-size size))
-     :line (if initial-contents 
-	       (make-double-array lsize :initial-contents initial-contents)
-	       (if initial-element
-		   (make-double-array lsize :initial-element (double initial-element))
-		   (make-double-array lsize)))
-     :type (or type
-	       (if max-size
-		   :mus-interp-linear
-		   :mus-interp-none)))))
+    (%make-delay
+     (floor size) ;size
+     (if initial-contents ;line
+	 (make-double-array lsize :initial-contents initial-contents)
+	 (if initial-element
+	     (make-double-array lsize :initial-element (double initial-element))
+	     (make-double-array lsize)))
+     0 ;loc
+     (and max-size (- max-size size)) ;zloc
+     lsize ;zsize
+     max-size ;zdly
+     (or type ;type
+	 (if max-size
+	     :mus-interp-linear
+	     :mus-interp-none)))))
 
 (defun tap (d &optional (offset 0.0))
   (if (delay-zdly d)
-      (if (= offset 0.0) 
+      (if (= offset 0.0)
 	  (aref (delay-line d) (delay-zloc d))
 	(array-interp (delay-line d) (- (delay-zloc d) offset) (delay-zsize d)))
-    (if (= offset 0.0) 
+    (if (= offset 0.0)
 	(aref (delay-line d) (delay-loc d))
       (aref (delay-line d) (floor (mod (- (delay-loc d) offset) (delay-size d)))))))
-	  
+
 (defun delay-tick (d input)
   (setf (aref (delay-line d) (delay-loc d)) (double input))
   (incf (delay-loc d))
@@ -84,46 +84,77 @@
     (delay-tick d input)))
 
 ;;;;all-pass
-(defstruct (all-pass (:include delay)))
+(defstruct (all-pass (:include delay)
+		     (:constructor %make-all-pass (size line loc zloc zsize zdly yscl xscl type))))
 
-(defun new-all-pass (feedback feedforward size initial-contents initial-element max-size type)
+(defun make-all-pass (feedback feedforward size initial-contents initial-element max-size type)
   (let ((lsize (round (or max-size size))))
-    (make-instance 'all-pass
-		   :loc 0
-		   :yscl feedback
-		   :xscl feedforward
-		   :size (floor size)
-		   :zsize lsize
-		   :zdly max-size
-		   :zloc (and max-size (- max-size size))
-		   :line (if initial-contents 
-			     (make-double-array lsize :initial-contents initial-contents)
-			     (if initial-element
-				 (make-double-array lsize :initial-element (double initial-element))
-				 (make-double-array lsize)))
-		   :type (or type
-			     (if max-size
-				 :mus-interp-linear
-				 :mus-interp-none)))))
+    (%make-all-pass
+     ;;size
+     (floor size)
+     ;;line
+     (if initial-contents
+	 (make-double-array lsize :initial-contents initial-contents)
+	 (if initial-element
+	     (make-double-array lsize :initial-element (double initial-element))
+	     (make-double-array lsize)))
+     ;;loc
+     0
+     ;;zloc
+     (and max-size (- max-size size))
+     ;;zsize
+     lsize
+     ;;zdly
+     max-size
+     ;;yscl
+     feedback
+     ;;xscl
+     feedforward
+     ;;type
+     (or type
+	 (if max-size
+	     :mus-interp-linear
+	     :mus-interp-none)))))
+
 (defun all-pass (d input &optional (pm 0.0))
   (let ((d-in (+ input (* (delay-yscl d) (tap d pm)))))
     (+ (delay d d-in pm)
        (* (delay-xscl d) d-in))))
 
+;;;;comb
+(defstruct (comb (:include delay)
+		 (:constructor %make-comb (loc xscl size zsize zdly zloc line type))))
+
+(defun make-comb (scaler size initial-contents initial-element max-size type)
+  (let ((lsize (round (or max-size size))))
+    (%make-comb
+     0
+     scaler
+     (floor size)
+     lsize
+     max-size
+     (and max-size (- max-size size))
+     (if initial-contents
+	 (make-double-array lsize :initial-contents initial-contents)
+	 (if initial-element
+	     (make-double-array lsize :initial-element (double initial-element))
+	     (make-double-array lsize)))
+     (or type
+	 (if max-size
+	     :mus-interp-linear
+	     :mus-interp-none)))))
+
+(defun comb (d input &optional (pm 0.0))
+  (delay d (+ input (* (delay-xscl d) (tap d pm)))))
 
 ;;;; asymmetric-fm
-(defstruct asymmetric-fm r freq ratio phase cosr sinr)
+(defstruct (asymmetric-fm (:constructor %make-asymmetric-fm (r freq ratio phase cosr sinr)))
+  r freq ratio phase cosr sinr)
 
-(defun new-asymmetric-fm (&key (frequency *default-frequency*) (initial-phase 0.0) (r 1.0) (ratio 1.0))
+(defun make-asymmetric-fm (&optional (frequency *default-frequency*) (initial-phase 0.0) (r 1.0) (ratio 1.0))
   (if (/= r 0.0)
-      (make-instance 'asymmetric-fm
-		     :r r
-		     :freq (hz->radians frequency)
-		     :ratio ratio
-		     :phase initial-phase
-		     :cosr (* .5 (- r (/ 1.0 r)))
-		     :sinr (* .5 (+ r (/ 1.0 r))))))
-			
+      (%make-asymmetric-fm r (hz->radians frequency) ratio initial-phase (* 0.5 (- r (/ 1.0 r))) (* 0.5 (+ r (/ 1.0 r))))))
+
 (defun asymmetric-fm (af index &optional (fm 0.0))
   (let* ((th (asymmetric-fm-phase af))
 	 (mth (* (asymmetric-fm-ratio af) th))
@@ -135,39 +166,15 @@
       (setf (asymmetric-fm-phase af) (mod (asymmetric-fm-phase af) +two-pi+)))
     result))
 
-;;;;comb
-(defstruct (comb (:include delay)))
-
-(defun new-comb (scaler size initial-contents initial-element max-size type)
-  (let ((lsize (round (or max-size size))))
-    (make-instance 'comb
-		   :loc 0
-		   :xscl scaler
-		   :size (floor size)
-		   :zsize lsize
-		   :zdly max-size
-		   :zloc (and max-size (- max-size size))
-		   :line (if initial-contents 
-			     (make-double-array lsize :initial-contents initial-contents)
-			     (if initial-element
-				 (make-double-array lsize :initial-element (double initial-element))
-				 (make-double-array lsize)))
-		   :type (or type
-			     (if max-size
-				 :mus-interp-linear
-				 :mus-interp-none)))))
-
-(defun comb (d input &optional (pm 0.0))
-  (delay d (+ input (* (delay-xscl d) (tap d pm)))))
-
 ;;;;oscil
-(defstruct oscil freq phase)
+(defstruct (oscil (:constructor %make-oscil (freq phase)))
+  freq phase)
 
-(defun new-oscil (&optional (frequency 440.0) (initial-phase 0.0))
-  (make-oscil :freq (hz->radians frequency) :phase initial-phase))
+(defun make-oscil (&optional (frequency 440.0) (initial-phase 0.0))
+  (%make-oscil (hz->radians frequency) initial-phase))
 
 (defun oscil (gen &optional (fm-input 0.0) (pm-input 0.0))
-  (prog1 
+  (prog1
       (sin (+ (oscil-phase gen) pm-input))
     (incf (oscil-phase gen) (+ (oscil-freq gen) fm-input))
     ;; if we were being extremely careful, we'd add the fm-input into the sin call at the start too.
@@ -175,25 +182,26 @@
       (setf (oscil-phase gen) (mod (oscil-phase gen) +two-pi+)))))
 
 ;;;;triangle-wave
-(defstruct triangle-wave current-value freq phase base)
-	
+(defstruct (triangle-wave (:constructor %make-triangle-wave (current-value freq phase base)))
+  current-value freq phase base)
+
 (defun fix-up-phase (s)
   (if (plusp (triangle-wave-phase s))
       (loop while (>= (triangle-wave-phase s) +two-pi+) do (decf (triangle-wave-phase s) +two-pi+))
     (loop while (minusp (triangle-wave-phase s)) do (incf (triangle-wave-phase s) +two-pi+))))
 
 (defun tri-val (amplitude phase)
-  (* amplitude (if (< phase (/ pi 2.0)) phase 
+  (* amplitude (if (< phase (/ pi 2.0)) phase
 		 (if (< phase (/ (* 3.0 pi) 2.0))
 		     (- pi phase)
 		   (- phase +two-pi+)))))
 
-(defun new-triangle-wave (&key (frequency *default-frequency*) (amplitude 1.0) (initial-phase 0.0))
-  (make-instance 'triangle-wave
-		 :current-value (/ (tri-val amplitude initial-phase) (/ pi 2.0))
-		 :base (/ (* 2 amplitude) pi)
-		 :phase initial-phase
-		 :freq (hz->radians frequency)))
+(defun make-triangle-wave (&optional (frequency *default-frequency*) (amplitude 1.0) (initial-phase 0.0))
+  (%make-triangle-wave
+   (/ (tri-val amplitude initial-phase) (/ pi 2.0))
+   (hz->radians frequency)
+   initial-phase
+   (/ (* 2 amplitude) pi)))
 
 (defun triangle-wave (s &optional (fm 0.0))
   (prog1
@@ -205,35 +213,37 @@
     (setf (triangle-wave-current-value s) (tri-val (triangle-wave-base s) (triangle-wave-phase s)))))
 
 ;;;;square-wave
-(defstruct (square-wave (:include triangle-wave))
-  (width pi))
+(defstruct (square-wave (:include triangle-wave)
+			(:constructor %make-square-wave (current-value freq phase base width)))
+  (width))
 
-(defun new-square-wave (&key (frequency *default-frequency*) (amplitude 1.0) (initial-phase 0.0))
-  (make-instance 'square-wave
-		 :current-value (if (< initial-phase pi) 0.0 amplitude)
-		 :base amplitude
-		 :phase initial-phase
-		 :width pi
-		 :freq (hz->radians frequency)))
+(defun make-square-wave (&optional (frequency *default-frequency*) (amplitude 1.0) (initial-phase 0.0))
+  (%make-square-wave
+   (if (< initial-phase pi) 0.0 amplitude)
+   (hz->radians frequency)
+   initial-phase
+   amplitude
+   pi))
 
 (defun square-wave (s &optional (fm 0.0))
   (prog1
       (square-wave-current-value s)
     (incf (square-wave-phase s) (+ (square-wave-freq s) fm))
     (if (or (minusp (square-wave-phase s))
-	    (>= (square-wave-phase s) +two-pi+)) 
+	    (>= (square-wave-phase s) +two-pi+))
 	(fix-up-phase s))
     (setf (square-wave-current-value s) (if (< (square-wave-phase s) (square-wave-width s)) (square-wave-base s) 0.0))))
 
 ;;;;sawtooth-wave
-(defstruct (sawtooth-wave (:include triangle-wave)))
+(defstruct (sawtooth-wave (:include triangle-wave)
+			  (:constructor %make-sawtooth-wave (current-value freq phase base))))
 
-(defun new-sawtooth-wave (&key (frequency *default-frequency*) (amplitude 1.0) (initial-phase pi))
-  (make-instance 'sawtooth-wave
-		 :current-value (* amplitude (/ (- initial-phase pi) pi))
-		 :base (/ amplitude pi)
-		 :phase initial-phase
-		 :freq (hz->radians frequency)))
+(defun make-sawtooth-wave (&optional (frequency *default-frequency*) (amplitude 1.0) (initial-phase pi))
+  (%make-sawtooth-wave
+		 (* amplitude (/ (- initial-phase pi) pi))
+		 (hz->radians frequency)
+		 initial-phase
+		 (/ amplitude pi)))
 
 (defun sawtooth-wave (s &optional (fm 0.0))
   (prog1
@@ -246,14 +256,15 @@
 
 
 ;;;;pulse-train
-(defstruct (pulse-train (:include triangle-wave)))
+(defstruct (pulse-train (:include triangle-wave)
+			(:constructor %make-pulse-train (current-value freq phase base))))
 
-(defun new-pulse-train (&key (frequency *default-frequency*) (amplitude 1.0) (initial-phase +two-pi+))
-  (make-instance 'pulse-train
-		 :current-value 0.0
-		 :base amplitude		; another version alternates sign
-		 :phase initial-phase		; this will give us an immediate pulse
-		 :freq (hz->radians frequency)))
+(defun make-pulse-train (&optional (frequency *default-frequency*) (amplitude 1.0) (initial-phase +two-pi+))
+  (%make-pulse-train
+   0.0
+   (hz->radians frequency)
+   initial-phase	; this will give us an immediate pulse
+   amplitude))		; another version alternates sign
 
 (defun pulse-train (s &optional (fm 0.0))
   (prog1
